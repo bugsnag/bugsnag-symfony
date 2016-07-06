6b825732e65a3f63d0984161bebb293088cde614
Graham Campbell <graham@bugsnag.com>
2016-07-06 12:06:16 -0700

    Fixed typos

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bugnsag\BugnsagBundle\DependencyInjection;
+namespace Bugsnag\BugsnagBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
