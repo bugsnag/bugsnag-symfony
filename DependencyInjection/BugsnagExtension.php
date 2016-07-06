41f25bf3b353b87ee45db9f04f5052a7d1d97cb3
Graham Campbell <graham@alt-three.com>
2016-07-06 15:05:51 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- DependencyInjection/BugsnagExtension.php
+++ DependencyInjection/BugsnagExtension.php
@@ -2,10 +2,10 @@
 
 namespace Bugsnag\BugsnagBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class BugsnagExtension extends Extension
 {
@@ -28,7 +28,7 @@ class BugsnagExtension extends Extension
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
         $loader->load('services.yml');
 
