597c2ca88eac9dfc246221a796c9efdc382f0806
Graham Campbell <graham@bugsnag.com>
2016-08-23 09:44:23 +0100

    Implemented the config reading

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,35 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('bugsnag');
 
-        // TODO
+        $rootNode
+            ->children()
+                ->scalarNode('api_key')
+                    ->defaultValue(getenv('BUGSNAG_API_KEY'))
+                ->end()
+                ->scalarNode('endpoint')
+                    ->defaultValue(getenv('BUGSNAG_ENDPOINT'))
+                ->end()
+                ->scalarNode('callbacks')
+                    ->defaultValue(true)
+                ->end()
+                ->scalarNode('strip_path')
+                    ->defaultNull()
+                ->end()
+                ->scalarNode('project_root')
+                    ->defaultNull()
+                ->end()
+                ->scalarNode('exception_listener')
+                    ->defaultValue('Bugsnag\BugsnagBundle\EventListener\ExceptionListener')
+                ->end()
+                ->arrayNode('notify_release_stages')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(array())
+                ->end()
+                ->arrayNode('filters')
+                    ->prototype('scalar')->end()
+                    ->defaultValue(['password'])
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
