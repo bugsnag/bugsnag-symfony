438f2f5ffe723c57221474f0beb19a8b655048d9
Graham Campbell <graham@bugsnag.com>
2016-08-23 17:05:21 +0100

    Added demo app

---
+++ example/symfony3/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Exception;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+    /**
+     * @Route("test", name="test")
+     */
+    public function testAction(Request $request)
+    {
+        throw new Exception('Something bad happened!');
+    }
+}
