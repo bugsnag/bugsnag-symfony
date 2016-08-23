b43f91c865e28432021beb9c0b163f8e9dbab92e
Graham Campbell <graham@bugsnag.com>
2016-08-23 17:12:34 +0100

    Removed old comments

--- example/symfony3/app/autoload.php
+++ example/symfony3/app/autoload.php
@@ -3,9 +3,6 @@
 use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-/**
- * @var ClassLoader
- */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
--- example/symfony3/web/app.php
+++ example/symfony3/web/app.php
@@ -2,9 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-/*
- * @var Composer\Autoload\ClassLoader
- */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
