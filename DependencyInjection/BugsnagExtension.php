39e02966a6922a6a80c1ba86b23d3b18b1a8da83
Graham Campbell <graham@alt-three.com>
2016-09-02 13:47:13 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- DependencyInjection/BugsnagExtension.php
+++ DependencyInjection/BugsnagExtension.php
@@ -2,7 +2,6 @@
 
 namespace Bugsnag\BugsnagBundle\DependencyInjection;
 
-use Bugsnag\BugsnagBundle\BugsnagBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
