d226f802c6c80e3a52c9bf56429fe8190accf32e
Alex Moinet <alex@bugsnag.com>
2017-09-19 10:49:48 +0100

    Added tests for listener

---
+++ Tests/Listener/BugsnagListenerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Bugsnag\BugsnagBundle\Tests\Listener;
+
+use Bugsnag\Client;
+use Bugsnag\Report;
+use Bugsnag\BugsnagBundle\Request\SymfonyResolver;
+use Bugsnag\BugsnagBundle\EventListener\BugsnagListener;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Console\Event\ConsoleExceptionEvent;
+use GrahamCampbell\TestBenchCore\MockeryTrait;
+use Mockery;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ReportStub {
+    const MIDDLEWARE_HANDLER = 'middleware_handler';
+}
+
+class BugsnagListenerTest extends TestCase
+{
+    use MockeryTrait;
+
+    public function testOnKernelException()
+    {
+        # Create mocks
+        $report = Mockery::namedMock(Report::class, ReportStub::class);
+        $client = Mockery::mock(Client::class);
+        $event = Mockery::mock(GetResponseForExceptionEvent::class);
+        $resolver = Mockery::mock(SymfonyResolver::class);
+
+        # Setup responses
+        $event->shouldReceive('getException')->once()->andReturn('exception');
+        $report->shouldReceive('fromPHPThrowable')
+            ->with('config', 'exception', 'middleware_handler', ['name' => 'symfony'])
+            ->once()
+            ->andReturn($report);
+        $client->shouldReceive('getConfig')->once()->andReturn('config');
+        $client->shouldReceive('notify')->once()->with($report);
+        
+        # Initiate test
+        $listener = new BugsnagListener($client, $resolver, true);
+        $listener->onKernelException($event);
+    }
+
+    public function testOnConsoleException()
+    {
+        # Create mocks
+        $report = Mockery::namedMock(Report::class, ReportStub::class);
+        $client = Mockery::mock(Client::class);
+        $event = Mockery::mock(ConsoleExceptionEvent::class);
+        $resolver = Mockery::mock(SymfonyResolver::class);
+
+        # Setup responses
+        $event->shouldReceive('getException')->once()->andReturn('exception');
+        $event->shouldReceive('getCommand')->once()->andReturn($event);
+        $event->shouldReceive('getName')->once()->andReturn('test');
+        $event->shouldReceive('getExitCode')->once()->andReturn(1);
+        $report->shouldReceive('fromPHPThrowable')
+            ->with('config', 'exception', 'middleware_handler', ['name' => 'symfony'])
+            ->once()
+            ->andReturn($report);
+        $client->shouldReceive('getConfig')->once()->andReturn('config');
+        $client->shouldReceive('notify')->once()->with($report, Mockery::any());
+        
+        # Initiate test
+        $listener = new BugsnagListener($client, $resolver, true);
+        $listener->onConsoleException($event);
+    }
+}
