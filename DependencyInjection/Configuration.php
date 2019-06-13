6e9292d4e16a391863c2c4790478d129fb431759
iliabox <iliabox@users.noreply.github.com>
2019-06-14 01:09:50 +0300

    Fixed deprecated since Symfony 4.3 ($rootNode)
    In the previous commit, this important line was mistakenly omitted.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('bugsnag');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
