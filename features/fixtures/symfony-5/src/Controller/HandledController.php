183e8cc9556bea49be1d834fba472293ec415e68
Joe Haines <hello@joehaines.co.uk>
2021-07-06 15:07:49 +0100

    Add tests for handled errors from views

--- features/fixtures/symfony-5/src/Controller/HandledController.php
+++ features/fixtures/symfony-5/src/Controller/HandledController.php
@@ -6,8 +6,9 @@ use LogicException;
 use Bugsnag\Client;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HandledController
+class HandledController extends AbstractController
 {
     /**
      * @Route("/handled/controller/exception")
@@ -27,5 +28,21 @@ class HandledController
         $bugsnag->notifyError('Handled error', 'This is a handled error');
 
         return new Response(__METHOD__);
+    }
+
+    /**
+     * @Route("/handled/view/exception")
+     */
+    public function handledExceptionInView(): Response
+    {
+        return $this->render('handled/exception.html.twig');
+    }
+
+    /**
+     * @Route("/handled/view/error")
+     */
+    public function handledErrorInView(): Response
+    {
+        return $this->render('handled/error.html.twig');
     }
 }
--- features/fixtures/symfony-5/src/Twig/BugsnagExtension.php
+++ features/fixtures/symfony-5/src/Twig/BugsnagExtension.php
@@ -4,11 +4,19 @@ namespace App\Twig;
 
 use Closure;
 use LogicException;
+use Bugsnag\Client;
 use Twig\TwigFunction;
 use Twig\Extension\AbstractExtension;
 
 class BugsnagExtension extends AbstractExtension
 {
+    private Client $bugsnag;
+
+    public function __construct(Client $bugsnag)
+    {
+        $this->bugsnag = $bugsnag;
+    }
+
     public function getFunctions()
     {
         return [
@@ -19,6 +27,14 @@ class BugsnagExtension extends AbstractExtension
             new TwigFunction(
                 'trigger_unhandled_error',
                 Closure::fromCallable([$this, 'triggerUnhandledError'])
+            ),
+            new TwigFunction(
+                'trigger_handled_exception',
+                Closure::fromCallable([$this, 'triggerHandledException'])
+            ),
+            new TwigFunction(
+                'trigger_handled_error',
+                Closure::fromCallable([$this, 'triggerHandledError'])
             ),
         ];
     }
@@ -31,5 +47,15 @@ class BugsnagExtension extends AbstractExtension
     private function triggerUnhandledError()
     {
         abcxyz();
+    }
+
+    private function triggerHandledException()
+    {
+        $this->bugsnag->notifyException(new LogicException('Handled exception'));
+    }
+
+    private function triggerHandledError()
+    {
+        $this->bugsnag->notifyError('A handled error', 'handled');
     }
 }
