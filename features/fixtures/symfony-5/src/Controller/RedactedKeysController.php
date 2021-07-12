f3ea1c55dfa5e3aecb0aa5a4f87b678d4bfe6798
Joe Haines <hello@joehaines.co.uk>
2021-07-12 09:25:31 +0100

    Remove unused imports

--- features/fixtures/symfony-5/src/Controller/RedactedKeysController.php
+++ features/fixtures/symfony-5/src/Controller/RedactedKeysController.php
@@ -3,20 +3,17 @@
 namespace App\Controller;
 
 use Exception;
-use LogicException;
 use Bugsnag\Client;
 use Bugsnag\Report;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RedactedKeysController
 {
     /**
      * @Route("/redacted-keys")
      */
-    public function __invoke(Request $request, Client $bugsnag): Response
+    public function __invoke(Client $bugsnag): Response
     {
         $bugsnag->notifyException(
             new Exception('This is a handled exception'),
