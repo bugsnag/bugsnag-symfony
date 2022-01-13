9a4da1038be37619f2819c80fe260760c94af5a5
Joe Haines <hello@joehaines.co.uk>
2022-01-13 17:21:01 +0000

    Allow skipping tests on Symfony <= 10

--- features/support/env.rb
+++ features/support/env.rb
@@ -29,7 +29,7 @@ Maze.hooks.before do
   Symfony.reset!
 end
 
-(2..5).each do |tag_version|
+(2..10).each do |tag_version|
   Before("@not_symfony_#{tag_version}") do
     skip_this_scenario if Symfony.version == tag_version
   end
