c3d1f4ba2369ebe69ca040cf50ddd7f37d0cc844
Joe Haines <hello@joehaines.co.uk>
2022-01-14 11:27:36 +0000

    Add Maze Runner test for user ID detection
    This is only running on Symfony 4 for now; other versions will be
    added separately

---
+++ features/fixtures/symfony-4/src/Security/QueryStringAuthenticator.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class QueryStringAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * Called on every request to decide if this authenticator should be
+     * used for the request. Returning `false` will cause this authenticator
+     * to be skipped.
+     */
+    public function supports(Request $request): bool
+    {
+        return $request->query->has('name');
+    }
+
+    /**
+     * Called on every request. Return whatever credentials you want to
+     * be passed to getUser() as $credentials.
+     */
+    public function getCredentials(Request $request)
+    {
+        return $request->query->get('name');
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
+    {
+        if (null === $credentials) {
+            return null;
+        }
+
+        return $userProvider->loadUserByUsername($credentials);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return true;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?Response
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     */
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        $data = [
+            'message' => 'Authentication Required'
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supportsRememberMe(): bool
+    {
+        return false;
+    }
+}
---
+++ features/user-detection.feature
@@ -0,0 +1,41 @@
+Feature: The current user ID should be detected automatically
+
+@not_symfony_2
+@not_symfony_5
+@not_symfony_6
+Scenario: The user ID of 'abc' should be detected when they are signed in
+  Given I start the symfony fixture
+  When I navigate to the route "/unhandled/controller/exception?name=abc"
+  Then I wait to receive an error
+  And the error is valid for the error reporting API version "4.0" for the "Bugsnag Symfony" notifier
+  And the event "user.id" equals "abc-123"
+  And the exception "errorClass" equals "RuntimeException"
+  And the exception "message" equals "Crashing exception!"
+  And the event "metaData.request.httpMethod" equals "GET"
+  And the event "metaData.request.url" ends with "/unhandled/controller/exception?name=abc"
+  And the event "app.type" equals "HTTP"
+  And the event "context" equals "GET /unhandled/controller/exception"
+  And the event "severity" equals "error"
+  And the event "unhandled" is true
+  And the event "severityReason.type" equals "unhandledExceptionMiddleware"
+  And the event "severityReason.attributes.framework" equals "Symfony"
+
+@not_symfony_2
+@not_symfony_5
+@not_symfony_6
+Scenario: The user ID of 'xyz' should be detected when they are signed in
+  Given I start the symfony fixture
+  When I navigate to the route "/unhandled/controller/exception?name=xyz"
+  Then I wait to receive an error
+  And the error is valid for the error reporting API version "4.0" for the "Bugsnag Symfony" notifier
+  And the event "user.id" equals "xyz-789"
+  And the exception "errorClass" equals "RuntimeException"
+  And the exception "message" equals "Crashing exception!"
+  And the event "metaData.request.httpMethod" equals "GET"
+  And the event "metaData.request.url" ends with "/unhandled/controller/exception?name=xyz"
+  And the event "app.type" equals "HTTP"
+  And the event "context" equals "GET /unhandled/controller/exception"
+  And the event "severity" equals "error"
+  And the event "unhandled" is true
+  And the event "severityReason.type" equals "unhandledExceptionMiddleware"
+  And the event "severityReason.attributes.framework" equals "Symfony"
