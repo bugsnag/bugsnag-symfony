b1c06a221fd9e6775996038794924269245b3b90
mclack <milo.clack@smartbear.com>
2023-05-18 11:32:31 +0100

    new example app

---
+++ example/symfony62/src/Controller/DefaultController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use Bugsnag\Report;
+use RuntimeException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    private $bugsnag;
+
+    public function __construct(\Bugsnag\Client $bugsnag)
+    {
+        $this->bugsnag = $bugsnag;
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return new Response('Welcome to the BugSnag Symfony 6 example. Visit the
+         file "src/Controller/DefaultController" to see how certain functions 
+        are implemented, and routes they can be tested on.');
+    }
+
+    /**
+     * @Route("/crash/", name="crash")
+     */
+    public function crash()
+    {
+        throw new RuntimeException('It crashed! Go to your BugSnag dashboard to view the exception');
+    }
+
+    /**
+     * @Route("/callback/", name="callback")
+     */
+    public function callback()
+    {
+        $this->bugsnag->registerCallback(function (Report $report) {
+            $report->setMetaData([
+                'account' => [
+                    'name' => 'Acme Co.',
+                    'paying_customer' => true,
+                ],
+            ]);
+        });
+
+        throw new RuntimeException('It crashed!  Go to your BugSnag dashboard to view the exception and metadata');
+    }
+
+    /**
+     * @Route("/notify/", name="notify")
+     */
+    public function notify()
+    {
+        $this->bugsnag->notifyException(new RuntimeException("It didn't crash!"));
+
+        return new Response("It didn't crash, but check your BugSnag dashboard for the manual notification");
+    }
+
+    /**
+     * @Route("/metadata/", name="metadata")
+     */
+    public function metadata()
+    {
+        $this->bugsnag->notifyException(new RuntimeException("It didn't crash, with metadata!"), function (Report $report) {
+            $report->setMetaData([
+                'diagnostics' => [
+                    'error' => 'RuntimeException',
+                    'state' => 'Caught',
+                ],
+            ]);
+        });
+
+        return new Response("It didn't crash, but check your BugSnag dashboard for the manual notification with additional metadata");
+    }
+
+    /**
+     * @Route("/severity/", name="severity")
+     */
+    public function severity()
+    {
+        $this->bugsnag->notifyException(new RuntimeException("It didn't crash, with severity!"), function (Report $report) {
+            $report->setSeverity('info');
+        });
+
+        return new Response("It didn't crash, but check your BugSnag dashboard for the manual notification, and check the severity of the report");
+    }
+}
