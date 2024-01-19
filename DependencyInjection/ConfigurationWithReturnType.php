970bb9cfbb9641a59de7477abc9b4bdbbc1fb523
Joe Haines <hello@joehaines.co.uk>
2024-01-19 10:43:32 +0000

    Fix 'Class Configuration not found' error
    This happens when using composer's '--classmap-authoritative' option

--- DependencyInjection/ConfigurationWithReturnType.php
+++ DependencyInjection/ConfigurationWithReturnType.php
@@ -4,9 +4,9 @@
  * This file declares a Configuration class with a return type on the
  * 'getConfigTreeBuilder' for compatibility with Symfony 7+
  *
- * This is required to be in a separate file with Configuration.php requiring it
- * at runtime so that we can maintain compatibility with PHP 5, where return
- * types are not supported
+ * This is required to be in a separate file with
+ * 'create-configuration-class-alias.php' requiring it at runtime so that we can
+ * maintain compatibility with PHP 5 where return types are not supported
  */
 
 namespace Bugsnag\BugsnagBundle\DependencyInjection;
@@ -14,7 +14,7 @@ namespace Bugsnag\BugsnagBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class Configuration extends BaseConfiguration implements ConfigurationInterface
+class ConfigurationWithReturnType extends BaseConfiguration implements ConfigurationInterface
 {
     /**
      * Get the configuration tree builder.
